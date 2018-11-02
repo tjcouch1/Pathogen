d70c12ec8032bac72579fc425070f3ca1bd84440
ANIMLAB/tjc0014 <tjc0014@uah.edu>
2018-11-02 15:41:10 -0500

    Fixed quarantine killfeed bug, removed player camera sync over network, set Lobby_old to default

--- Assets/Scripts/KillFeed.cs
+++ Assets/Scripts/KillFeed.cs
@@ -17,22 +17,15 @@ public class KillFeed : MonoBehaviour {
     public void OnKill(string player, string source)
     {
         Player p = GameManager.getPlayer(player);
+		Player s = GameManager.getPlayer(source);
 
-        try
-        {
-           Player s = GameManager.getPlayer(source);
+		GameObject go = Instantiate(killFeedItemPrefab, this.transform);
+		if (s != null)
+			go.GetComponent<KillFeedElement>().Setup(p.username, s.username);
+		else
+			go.GetComponent<KillFeedElement>().Setup(p.username, source);
 
-            GameObject go = Instantiate(killFeedItemPrefab, this.transform);
-            go.GetComponent<KillFeedElement>().Setup(p.username, s.username);
-            go.transform.SetAsFirstSibling();
-
-            Destroy(go, killFeedTimeout);
-        }
-        catch (KeyNotFoundException)
-        {
-            GameObject go = Instantiate(killFeedItemPrefab, this.transform);
-            go.GetComponent<KillFeedElement>().Setup(p.username, source);
-            go.transform.SetAsFirstSibling();
-        }
+		go.transform.SetAsFirstSibling();
+		Destroy(go, killFeedTimeout);
     }
 }
