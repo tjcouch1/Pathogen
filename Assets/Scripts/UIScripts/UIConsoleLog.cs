82e78c27ab545ec8fc0418f5caf9b0ff7bf2ca65
ANIMLAB/tjc0014 <tjc0014@uah.edu>
2018-11-10 12:04:11 -0600

    Added on screen debug logger

---
+++ Assets/Scripts/UIScripts/UIConsoleLog.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+ using System.Collections;
+ 
+ public class UIConsoleLog : MonoBehaviour
+ {
+     string myLog;
+		 public UnityEngine.UI.Text logText;
+     Queue myLogQueue = new Queue();
+ 
+     void OnEnable () {
+         Application.logMessageReceived += HandleLog;
+     }
+     
+     void OnDisable () {
+         Application.logMessageReceived -= HandleLog;
+     }
+ 
+     void HandleLog(string logString, string stackTrace, LogType type){
+         myLog = logString;
+         string newString = "\n [" + type + "] : " + myLog;
+         myLogQueue.Enqueue(newString);
+         if (type == LogType.Exception)
+         {
+             newString = "\n" + stackTrace;
+             myLogQueue.Enqueue(newString);
+         }
+         myLog = string.Empty;
+         foreach(string mylog in myLogQueue){
+             myLog += mylog;
+         }
+
+				 logText.text = myLog;
+     }
+ }
