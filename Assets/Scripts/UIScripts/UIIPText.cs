edd97daf974bd0a09c9f0f933102c4d3571cdd24
ANIMLAB/tjc0014 <tjc0014@uah.edu>
2018-11-28 15:51:00 -0600

    Made splash screens shorter, added ip text to pause menu, added "Scroll to Swap" to PlayerUI

---
+++ Assets/Scripts/UIScripts/UIIPText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class UIIPText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text>().text += NetworkManager.singleton.networkAddress;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
